8df0554140d6a79cfa9447c3c88dad3a0f85d4ce
Eric Range <mail@eric-range.de>
2015-10-27 09:16:00 +0100

    Add content model and routes, closes #3

--- server/api/index.js
+++ server/api/index.js
@@ -1,18 +1,26 @@
 "use strict";
 
-import User from './models/user';
+import User    from './models/user';
+import Content from './models/content';
 
-const ROUTE_USER    = /\/user/;
-const ROUTE_USER_ID = /\/user\/([^\/]+)\/?/;
+const ROUTE_USER       = /\/user/;
+const ROUTE_USER_ID    = /\/user\/([^\/]+)\/?/;
+const ROUTE_CONTENT    = /\/content/;
+const ROUTE_CONTENT_ID = /\/content\/([^\/]+)\/?/;
 
-export default async function(route, params = {method: 'GET'}) {
+export default async function(route, params = { method: 'GET' }) {
   let queryParams;
 
   switch (params.method.toUpperCase()) {
     case 'GET':
       /* Route: /user/:id */
       if (queryParams = route.match(ROUTE_USER_ID)) {
-        return (new User({id: queryParams[1]})).toJSON();
+        return (new User({ id: queryParams[1] })).toJSON();
+      }
+
+      /* Route: /content/:id */
+      if (queryParams = route.match(ROUTE_CONTENT_ID)) {
+        return (new Content({ id: queryParams[1] })).toJSON();
       }
 
       break;
@@ -20,6 +28,10 @@ export default async function(route, params = {method: 'GET'}) {
     case 'POST':
       /* Route: /user */
       if (route.match(ROUTE_USER)) {}
+
+      /* Route: /content */
+      if (route.match(ROUTE_CONTENT)) {}
+
       break;
   }
 
