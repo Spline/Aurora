ce1d06d93b0b2be452795fa5e25ced02662c818e
Eric Range <ericrange@users.noreply.github.com>
2015-10-29 10:51:11 +0100

    Fix JavaScript convention in Content.js

--- server/api/models/Content.js
+++ server/api/models/Content.js
@@ -3,7 +3,10 @@
 export default class Content {
   constructor(params = {}) {
     /* If there is no id specified, this must be new content. */
-    if (this.id = params.id || null) {
+    this.id = params.id;
+    if (this.id) 
+      /* Fetch data from database. */
+    } else {
       this.title = params.title;
       this.body  = params.body;
     }
