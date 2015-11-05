5e41744e51ac918b64761320b5b50ffe13fdd2fa
Eric Range <mail@eric-range.de>
2015-11-05 14:51:22 +0100

    Add shared regex

--- core/server/api/routes.js
+++ core/server/api/routes.js
@@ -1,13 +1,14 @@
 "use strict";
 
-const REGEX_NUMBER = '([0-9]{1,8})';
+let REGEX = require(__ROOT + 'shared/regex');
 
 const LOGIN = new RegExp(`/login`);
 
 const USER = new RegExp(`/user`);
-const USER_ID = new RegExp(`/user/${REGEX_NUMBER}`);
+const USER_ID = new RegExp(`/user/${REGEX.ID}`);
 
 const CONTENT = new RegExp(`/content`);
-const CONTENT_ID = new RegExp(`/content/${REGEX_NUMBER}`);
+const CONTENT_ID = new RegExp(`/content/${REGEX.ID}`);
+const CONTENT_URI = new RegExp(`/content/${REGEX.URI}`);
 
 export default { LOGIN, USER, USER_ID, CONTENT, CONTENT_ID }
---
+++ core/shared/regex/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const PATTERN = {
+  ID:    '([\d]{1,8})',
+  EMAIL: '^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$',
+  URI:   '([\w\d\-]{1,255})'
+};
+
+export default PATTERN
