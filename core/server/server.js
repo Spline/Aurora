a2d782dd51659414fa98c85e3da5d1efdd501d50
Pablo Sichert <mail@pablosichert.de>
2015-11-10 23:33:32 +0100

    Replicate previous behaviour, now with koa

--- core/server/server.js
+++ core/server/server.js
@@ -1,3 +1,5 @@
+// TODO: static file serving when developing
+
 "use strict";
 
 import { createStore } from 'redux';
@@ -5,9 +7,8 @@ import http            from 'http';
 import Koa             from 'koa';
 import nunjucks        from 'nunjucks';
 import riot            from 'riot';
-import serve           from 'koa-static';
+import serve           from 'koa-static-server';
 
-var config, api, backend, frontend, reducers, routes, database;
 var check = require(__ROOT + 'core/server/checks');
 
 export default async function() {
@@ -15,14 +16,15 @@ export default async function() {
   /* Check if everything is configured as it should be. */
   await check.config();
 
-  config   = require(__ROOT + 'config');
-  api      = require(__ROOT + 'core/server/api');
-  reducers = require(__ROOT + 'core/shared/reducers');
-  routes   = require(__ROOT + 'core/shared/routes');
-  database = require(__ROOT + 'core/server/database');
+  var config   = require(__ROOT + 'config');
+  var api      = require(__ROOT + 'core/server/api');
+  var reducers = require(__ROOT + 'core/shared/reducers');
+  var routes   = require(__ROOT + 'core/shared/routes');
+  var database = require(__ROOT + 'core/server/database');
 
-  backend  = require(__ROOT + `themes/backend/${config.theme.backend}/components/index.tag`);
-  frontend = require(__ROOT + `themes/frontend/${config.theme.frontend}/components/index.tag`);
+  // Require components that depend on riot being included
+  var backend  = require(__ROOT + `themes/backend/${config.theme.backend}/components/index.tag`);
+  var frontend = require(__ROOT + `themes/frontend/${config.theme.frontend}/components/index.tag`);
 
   /* Bootstrap */
   try {
@@ -34,47 +36,51 @@ export default async function() {
 
   }
 
-  var app = new Koa();
-  app.experimental = true;
+  try {
+    var app = new Koa();
+    app.experimental = true;
 
-  nunjucks.configure(__ROOT + 'core/server/views', {
-    autoescape: false
-  });
-
-  app.use(serve('public'));
-
-/*
-  app.use((req, res, next) => {
-    // Default state
-    req.state = {
-      activeRoute: routes.home,
-      user: null
-    };
-    next();
-  });
-
-  for (let route in routes) {
-    app.get(routes[route].path, (req, res, next) => {
-      req.state.activeRoute = routes[route];
-      next();
+    nunjucks.configure(__ROOT + 'core/server/views', {
+      autoescape: false
     });
+
+    app.use(async function(context, nextMiddleware) {
+      // Init the state object
+      context.state = {
+        url: context.req.url,
+        content: {},
+        user: null
+      };
+
+      return await nextMiddleware();
+    });
+
+    app.use(async function(context, nextMiddleware) {
+      context.state.content = await api(context.req.url);
+
+      return await nextMiddleware();
+    });
+
+    app.use(async function(context) {
+      var store = createStore(reducers, context.state);
+      var html = riot.render((!context.state.user ? frontend : backend), {
+        isClient: false,
+        routes: routes,
+        store: store,
+        state: store.getState()
+      });
+
+      var body = nunjucks.render('base.tmpl', {
+        html: html,
+        initialState: context.state
+      });
+
+      context.body = body;
+    });
+  } catch(exception) {
+    console.log(exception);
+
   }
-
-  app.use((req, res) => {
-    var store = createStore(reducers, req.state);
-    var html = riot.render((!req.state.user ? frontend : backend), {
-      isClient: false,
-      routes: routes,
-      store: store,
-      state: store.getState()
-    });
-
-    res.render('base', {
-      html: html,
-      initialState: req.state
-    });
-  });
-*/
 
   http.createServer(app.callback()).listen(config.ports.http);
   console.log('http server started on port ' + config.ports.http);
