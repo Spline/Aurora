2104aa1eee6b3dc6101d636aba00798c85b9c674
Eric Range <mail@eric-range.de>
2015-11-15 09:44:47 +0100

    Add sqlite support

--- config.example.js
+++ config.example.js
@@ -6,12 +6,26 @@ const config = {
   },
   defaultTitle: 'App',
   database: {
-    dialect:  'mysql', // Supported: mysql, postgres, sqlite, mariadb and mssql
-    host:     '',
-    port:     '3306',
-    user:     '',
-    pass:     '',
-    name:     ''
+    dialect: 'mysql', // Supported: mysql, postgres, sqlite, mariadb and mssql
+    mysql: {
+      host: '',
+      port: '3306',
+      user: '',
+      pass: '',
+      name: ''
+    },
+    postgres: {
+      host: '',
+      port: '5432',
+      user: '',
+      pass: '',
+      name: ''
+    },
+    mariadb: {},
+    mssql: {},
+    sqlite: {
+      storage: './data.db'
+    }
   },
   cache: {
     enabled: true
--- core/server/database/orm/connector.js
+++ core/server/database/orm/connector.js
@@ -4,19 +4,27 @@ import Sequelize from 'sequelize';
 
 let config = require(__ROOT + 'config');
 
-export default new Sequelize(config.database.name, config.database.user, config.database.pass, {
-  host: config.database.host,
-  port: config.database.port,
-  dialect: config.database.dialect,
-  logging: false,
-  pool: {
-    maxConnections: 10,
-    maxIdleTime: 50
-  },
-  define: {
-    charset: 'utf8',
-    collate: 'utf8_general_ci',
-    timestamps: true
-  },
-  language: 'en'
-});
+export default new Sequelize(
+  config.database[config.database.dialect].name,
+  config.database[config.database.dialect].user,
+  config.database[config.database.dialect].pass, {
+    dialect: config.database.dialect,
+    host: config.database[config.database.dialect].host,
+    port: config.database[config.database.dialect].port,
+
+    /* Sqlite only */
+    storage: config.database.sqlite.storage || '',
+
+    logging: false,
+    pool: {
+      minConnections: 5,
+      maxConnections: 50,
+      maxIdleTime: 1000
+    },
+    define: {
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+      timestamps: true
+    },
+    language: 'en'
+  });
